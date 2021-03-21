09f512f6660f310e1943d57ebe9d60597ffe3b4d
Dalston_IPL <ward.dalston@gmail.com>
2021-03-21 12:51:10 +0100

    Font adjustment function
    Added a function that changes the default ggplot theme.

--- R/initial_development.R
+++ R/initial_development.R
@@ -25,8 +25,21 @@ texsave <- function(filename, plot = last_plot(), width = 6.5,  ratio = .66, ...
 }
 
 
+# textheme (version 1) ----------------------------------------------------
+
+# this will eventually be the function that overrides the ggplot theme to use the font size from
+# the target document. Eventually it will use the target font as well but that is down the line
+# would also be good if there is a way to invisibly save the old options so that I can do something like
+# theme_reset().
+textheme <- function(size){
+  theme_update(text = element_text(size = size))
+}
+
+
 # testing -----------------------------------------------------------------
 
 # the first latex test example is on 8.5 x 11 paper with standard
 # vertical alignment and 1 inch margins.
 texsave("test_examples/test_1.pdf")
+
+texsave("test_examples/test_2.pdf", ratio = .25)
